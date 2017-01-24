9f395a5bc54317d13b23990c997976199a3e3e05
Vojtěch Šnajdr <vojtech.snajdr@techlib.cz>
2017-01-24 18:45:52 +0100

    Implemented methods for looking up the user in db

--- kolaborator.py
+++ kolaborator.py
@@ -22,20 +22,73 @@ class Kolaborator(object):
         process(item_id)
 
     def process(self, item_id):
+        """Further process the notification, get all the details and find the perpetrator"""
         notice = self.db.infringements.get(item_id)
         internal_ip = find_flow(notice.ip_address, notice.port, notice.source_timestamp)
+        eduroam, staff, user = find_radius(internal_ip, notice.source_timestamp)
+        if eduroam:
+            # send notification to users eduroam institution
+        else:
+            # write row into table and if more than 3 do something, else notify the user of inapropriate activity
+
 
     def find_flow(self, external_ip, port, timestamp):
-        
-        return internal_ip
+        """Look into netflow for internal ip address of the perpetrator"""
+
+        query = 'select * \
+                    from(\
+                        select \
+                            (flow.fields ->> \'sourceIPv4Address\'::text)::inet AS internal_addr,\
+                            flow."time" - (flow.uptime - ((flow.fields ->> \'flowStartSysUpTime\'::text)::double precision)) * \'00:00:00.001\'::interval AS start_time, \
+                            flow."time" - (flow.uptime - ((flow.fields ->> \'flowEndSysUpTime\'::text)::double precision)) * \'00:00:00.001\'::interval AS stop_time \
+                        from flow \
+                        where flow.fields @> \'{{"postNATSourceIPv4Address": "{1}" , "postNAPTSourceTransportPort": {2} }}\'::jsonb) AS times\
+                    where date_trunc(\'second\', times.start_time) <= \'{0}\' AND date_trunc(\'second\',times.stop_time + interval \'1 second\') >= \'{0}\'\
+                    '.format(timestamp, external_ip, port)
+
+        res = []
+
+        for row in self.db_flow.execute(query).fetchall():
+            res.append(dict(zip(row.keys(), row.values())))
+
+        return res[0]['internal_addr']
 
     def find_radius(self, internal_ip, timestamp):
+        """Find the user,to whom was the internal ip address leased"""
+        # look into radius if it is eduroam user, reader or employee
+        query = 'select \
+                    username, \
+                    realm, \
+                    user, \
+                    description \
+                from accounting \
+                where framed_ip = \'{0}\' and start_time <= \'{1}\' and update_time >= \'{1}\' \
+                    '.format(internal_ip, timestamp)
 
-        return eduroam, user_id
+        res = []
+
+        for row in db_radius.execute(query).fetchall():
+            res.append(dict(zip(row.keys(), row.values())))
+
+        if res[0]['user'] == None:
+            staff = False
+            user = res[0]['username']
+            if res[0]['realm'] = 'NULL':
+                eduroam = False
+            else:
+                eduroam = True
+        else:
+            staff = True
+            eduroam = False
+            user = res[0]['user']
+
+        return eduroam, staff, user
 
     def send_message(self):
+        """Send warning to the user / eduroam institution"""
         return
 
     def send_response(self):
+        """Reply to complainant"""
         return
 # vim:set sw=4 ts=4 et:
